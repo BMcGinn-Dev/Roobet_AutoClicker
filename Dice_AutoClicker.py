6ca1022b6076a46431ed31c40f2cd767a60c49b5
Benjamin McGinn <103227767+BMcGinn-Dev@users.noreply.github.com>
2023-01-11 15:20:52 -0600

    Create Dice_AutoClicker.py
    Simplest autoclicker on roobet, run on mac

---
+++ Dice_AutoClicker.py
@@ -0,0 +1,26 @@
+import time
+import pyautogui
+from PIL import Image
+
+#Setting the fail-safe --> drag to any corner to kill program or you have to restart computer
+pyautogui.FAILSAFE = True
+
+play_amount = 500
+
+successful_plays = 0
+
+#play btn coords
+play_bttn = (950, 600)
+
+for i in range(play_amount):
+    
+    if i > 0 and i % 10 == 0:
+        print("Number of successful plays: ", successful_plays)
+        print("________")
+    
+    
+    pyautogui.click(950, 600)
+    successful_plays += 1
+    time.sleep(1)
+
+print("Successful plays: ", successful_plays)
